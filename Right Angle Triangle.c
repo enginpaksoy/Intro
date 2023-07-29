f5d9fcaeb406e91f437f026fc47e451ec239ff80
enginpaksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-07-29 18:55:13 +0300

    Create Right Angle Triangle.c

---
+++ Right Angle Triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int row, column, totalRow;
+    printf("Enter total number of rows: ");
+    scanf("%d", &totalRow);
+    for(row = 1; row <= totalRow; row++){
+        for(column = 1; column <= row; column++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
