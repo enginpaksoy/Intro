853f5340aaa6cd031756d34696c817a4e57301bb
enginpaksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-07-31 11:02:40 +0300

    Even or Odd.c

---
+++ Even or Odd.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int isEven(int a);
+
+int main(){
+    int num;
+    printf("Enter series of integers (enter 0 to stop): ");
+
+    while (1){
+        scanf("%d", &num);
+
+        if(num == 0){
+            break;
+        }
+        if(isEven(num) == 1){
+            printf("%d is even.\n", num);
+        }
+        else{
+            printf("%d is odd.\n", num);
+        }
+    }
+    return 0;
+}
+
+int isEven(int a){
+    if(a % 2 == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
