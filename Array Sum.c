81676b2cef3c921b8ec2a87051974b7b89266d10
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-02 10:15:12 +0300

    Array Sum 2D.c

---
+++ Array Sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define ROW 5
+#define COL 5
+int main() {
+    // Write C code here
+    int a[5][5] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+    int sum = 0;
+
+    for(int i = 0; i < ROW; i++){
+        for(int j = 0; j < COL; j++){
+            sum += a[i][j];
+        }
+        printf("%d ", sum);
+        sum = 0;
+    }
+
+    return 0;
+}
