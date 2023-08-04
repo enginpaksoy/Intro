0b1e0be262ede6cfbc945f0161a4c3fffcc0c7bf
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-04 14:50:33 +0300

    Character of arrays.c

--- Character of arrays.c
+++ Character of arrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define SIZE 20
 
 int main(){
@@ -6,7 +7,7 @@ int main(){
     printf("%s", "Enter a array that no longer than 19 characters: ");
     scanf("%19s", string1);
     printf("Character of these arrays: ");
-    for(int i; i < SIZE && string1[i] != '\0'; i++){
+    for(int i = 0; i < strlen(string1); i++){
         printf("%c ", string1[i]);
     }
 }
