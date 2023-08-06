fd3be70fb0dd3f7b2dab2f8bf1fe0019092d8a92
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-06 19:32:06 +0300

    Prime Number.c

---
+++ Prime Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    printf("Enter a integer: ");
+    scanf("%d", &num);
+    for (int i = 1; i < num; ++i) {
+        if(num % i == 0){
+            printf("It's not a prime number.");
+            break;
+        }
+        else{
+            printf("It's a prime number.");
+        }
+    }
+}
