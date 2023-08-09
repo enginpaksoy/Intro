f9d8c48b344b20d5f7329281a8316fa46c8dd8de
Engin Paksoy <96542214+enginpaksoy@users.noreply.github.com>
2023-08-09 20:58:40 +0300

    Update Leap Year.c

--- Leap Year.c
+++ Leap Year.c
@@ -4,7 +4,13 @@ int main() {
     int year;
     printf("%s", "Enter the year: ");
     scanf("%d", &year);
-    if(year % 4 == 0){
+    if(year % 400 == 0){
+        printf("%s", "This is a leap year.");
+    }
+    else if(year % 100 == 0){
+        printf("%s", "This isn't a leap year.");
+    }
+    else if(year % 4 == 0){
         printf("%s", "This is a leap year.");
     }
     else{
